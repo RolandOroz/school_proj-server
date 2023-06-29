53e5d03be2a0bf05b6760f92820b1ef061597f8b
RolandOroz <rolandoroz@gmail.com>
2023-06-29 15:59:04 +0100

    clean up

--- config/rolesList.js
+++ config/rolesList.js
@@ -1,11 +1,9 @@
-import { rolesListSchema } from "../sqlEntities/rolesListEntity.js";
-
-const rc_1 =process.env.RC_1
-const rc_2 =process.env.RC_2
-const rc_3 =process.env.RC_3
+const rc_1 = process.env.RC_1;
+const rc_2 = process.env.RC_2;
+const rc_3 = process.env.RC_3;
 
 export const ROLES_LIST = {
-  "Admin": rc_1,
-  "Editor": rc_2,
-  "User": rc_3,
+  Admin: rc_1,
+  Editor: rc_2,
+  User: rc_3,
 };
