7116b4919fcd9c3e6ea81644728c541713eb0000
RolandOroz <rolandoroz@gmail.com>
2023-06-29 15:58:05 +0100

    adding .env to roles list

--- config/rolesList.js
+++ config/rolesList.js
@@ -1,7 +1,11 @@
-import { rolesListSchema } from "../sqlEntities/rolesListEntity";
+import { rolesListSchema } from "../sqlEntities/rolesListEntity.js";
+
+const rc_1 =process.env.RC_1
+const rc_2 =process.env.RC_2
+const rc_3 =process.env.RC_3
 
 export const ROLES_LIST = {
-  "Admin": 110,
-  "Editor": 210,
-  "User": 310,
+  "Admin": rc_1,
+  "Editor": rc_2,
+  "User": rc_3,
 };
--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -3,8 +3,11 @@ export const verifyRoles = (...allowedRoles) => {
   return (req, res, next) => {
     if (!req?.roles) return res.sendStatus(401);
     const rolesArray = [...allowedRoles];
-    const userRole = req.roles
+    const userRole = JSON.stringify(req.roles);
     const compare = rolesArray.includes(userRole);
+    console.log(rolesArray);
+    console.log(userRole);
+    console.log("compare",compare);
     if(compare === false )
     return res.sendStatus(401);
     next();
